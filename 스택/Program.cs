54cc609782d06f6b9fb2489d94320e871a8c18b5
ingihong06 <ingihong07@gmail.com>
2022-05-26 08:36:17 +0900

    초기 커밋.

---
+++ 스택/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+namespace 스택
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = int.Parse(Console.ReadLine());
+            int i = 0;
+            int[] arr = new int[index];
+            int j = 1;
+            Stack stack = new Stack();
+            while (i < index)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+                i++;
+            }
+            int arrnum = arr[0];
+            i = 1;
+            while (i <= arr.Length)
+            {
+                if (i <= arrnum)
+                {
+                    stack.Push(i);
+                    Console.WriteLine("+");
+                    i++;
+                } else
+                {
+                    Console.WriteLine("-");
+                    //Console.WriteLine(arrnum);
+                    arrnum = arr[j++];
+                    stack.Pop();
+                }
+            }
+            while (stack.Count > 0)
+            {
+                Console.WriteLine("-");
+                //Console.WriteLine(arrnum);
+                arrnum = arr[j++];
+                stack.Pop();
+            }
+        }
+    }
+}
